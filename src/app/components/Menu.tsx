e3422beac30a4a20f152f5c193a7665996862078
MagnusLidmyr <magnus.lidmyr@teliacompany.com>
2024-01-04 22:00:28 +0100

    fixed typo

--- src/app/components/Menu.tsx
+++ src/app/components/Menu.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Menu() {
   return (
     <nav className="flex justify-between mx-5 py-14 md:mx-8">
-      <h1>STOCKHOLM METALLSLIPERI</h1>
+      <h1>STOCKHOLMS METALLSLIPERI</h1>
       <Link href="#kontakt">KONTAKT</Link>
     </nav>
   );
